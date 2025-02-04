83a4fb0216ce83863c32c50b709945795ea71773
Matt Harper <matt.harper3415@gmail.com>
2025-02-03 21:52:39 -0700

    fix: disable suspend

--- mod/gnome.nix
+++ mod/gnome.nix
@@ -43,4 +43,9 @@
     layout = "us";
     variant = "";
   };
+
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
