45eb16a5297da646ee97c6eff25b208edb4fd88a
Matt Harper <matt.harper3415@gmail.com>
2025-02-04 19:15:13 -0700

    feat: tiling, clock, power settings

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -4,9 +4,17 @@
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
+      clock-format = "12h";
+      clock-show-weekday = true;
     };
     "org/gnome/desktop/wm/preferences" = {
       button-layout = ":minimize,maximize,close";
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
     };
   };
 }
