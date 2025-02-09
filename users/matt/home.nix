8788731bee552eb39dfa717676bab713b80273f1
Matt Harper <matt.harper3415@gmail.com>
2025-02-09 00:13:16 -0700

    chore: ripgrep goes in the nvim config 'that's right, the square hole'

--- users/matt/home.nix
+++ users/matt/home.nix
@@ -48,7 +48,6 @@
     p7zip
 
     # utils
-    ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
     eza # A modern replacement for ‘ls’
