43da3d0d4459047fbaa587d191140bf80514c38d
Matt Harper <matt.harper3415@gmail.com>
2025-08-13 15:42:40 -0600

    add polkit

--- mod/core/programs.nix
+++ mod/core/programs.nix
@@ -4,5 +4,8 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [ ];
   programs._1password.enable = true;
-  programs._1password-gui.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = ["matt"];
+  };
 }
