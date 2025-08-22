8ed136da16b62c6d384f1990f632c7857eb4cbc1
Matt Harper <matt.harper3415@gmail.com>
2025-08-22 16:32:30 -0600

    feat: rider and clion

--- mod/core/jetbrains.nix
+++ mod/core/jetbrains.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ jetbrains-toolbox ];
+  environment.systemPackages = with pkgs; [ jetbrains.rider jetbrains.clion ];
+
+  user.persist.directories = [ ".cache/JetBrains" ".config/JetBrains" ];
 }
