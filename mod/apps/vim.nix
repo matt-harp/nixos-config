dc336129597be8195e2161424859b671b2f41b2a
Matt Harper <matt.harper3415@gmail.com>
2025-10-22 10:33:32 -0600

    feat(nvim): snacks

--- mod/apps/vim.nix
+++ mod/apps/vim.nix
@@ -2,10 +2,12 @@
   config,
   lib,
   inputs,
+  pkgs,
   ...
 }:
 let
   cfg = config.apps.vim;
+  mkLua = lib.generators.mkLuaInline;
 in
 {
   options.apps.vim = {
@@ -17,6 +19,11 @@ in
   ];
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      ripgrep
+      fd
+    ];
+
     programs.nvf = {
       enable = true;
 
@@ -28,8 +35,37 @@ in
             style = "mocha";
           };
 
+          options = {
+            showmode = false; # commandline mode disable
+          };
+
+          keymaps = [
+            {
+              mode = "n";
+              key = "<leader><space>";
+              action = "function() Snacks.picker.smart() end";
+              lua = true;
+              desc = "Smart Find Files";
+            }
+            {
+              mode = "n";
+              key = "<leader>/";
+              action = "function() Snacks.picker.grep() end";
+              lua = true;
+              desc = "Grep";
+            }
+            {
+              mode = "n";
+              key = "<leader>sk";
+              action = "function() Snacks.picker.keymaps() end";
+              lua = true;
+              desc = "Keymaps";
+            }
+          ];
+
+          autopairs.nvim-autopairs.enable = true;
           statusline.lualine.enable = true;
-          telescope.enable = true;
+          # telescope.enable = true;
           autocomplete.nvim-cmp.enable = true;
 
           binds = {
@@ -39,11 +75,20 @@ in
           utility = {
             sleuth.enable = true;
             direnv.enable = true;
+            snacks-nvim = {
+              enable = true;
+              setupOpts = {
+                picker = {
+                  enabled = true;
+                };
+                explorer = {
+                  enabled = true;
+                };
+              };
+            };
           };
 
-          filetree = {
-            neo-tree.enable = true;
-          };
+          treesitter.fold = true;
 
           lsp = {
             enable = true;
@@ -59,6 +104,10 @@ in
             ts.enable = true;
             rust.enable = true;
             clang.enable = true;
+            odin = {
+              enable = true;
+              lsp.package = [ "ols" ]; # use $PATH-based LS
+            };
           };
         };
       };
